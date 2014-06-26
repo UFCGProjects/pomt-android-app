5d0e3e2b871831ecde68fbe08489c1ad868365ef
Dirceu Medeiros <dirceu.teixeira@embedded.ufcg.edu.br>
2014-06-26 13:55:52 -0300

    Testes do robotium

--- Testpotm-android-app/src/com/potm_android_app/test/TestMain.java
+++ Testpotm-android-app/src/com/potm_android_app/test/TestMain.java
@@ -1,30 +1,38 @@
 package com.potm_android_app.test;
 
+import com.potm_android_app.R;
+import com.potm_android_app.screen.auth.AuthActivity;
 import com.potm_android_app.screen.main.MainActivity;
 import com.robotium.solo.Solo;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
-public class TestMain extends ActivityInstrumentationTestCase2<MainActivity> {
+public class TestMain extends ActivityInstrumentationTestCase2<AuthActivity> {
 
-	private Solo solo;
+	private Solo mSolo;
 
 	public TestMain() {
-		super(MainActivity.class);
+		super(AuthActivity.class);
 	}
 
 	public void setUp() throws Exception {
-		solo = new Solo(getInstrumentation(), getActivity());
+		mSolo = new Solo(getInstrumentation(), getActivity());
 	}
 
-	public void testPreferenceIsSaved() throws Exception {
+	public void testLogin() throws Exception {
+		View buttonSignIn = mSolo.getView(R.id.buttonContinuar);
+		
+	
+		mSolo.clickOnView(buttonSignIn);
+		
+	///	mSolo.assertCurrentActivity("OK",MainActivity.class);
+
 
 	}
-	
-	
 
 	@Override
 	public void tearDown() throws Exception {
-		solo.finishOpenedActivities();
+		mSolo.finishOpenedActivities();
 	}
 }
