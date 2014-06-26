8a09e203bbea412f2a970b81c9caa646c86f84f7
matheusbs <matheus.ufcg@gmail.com>
2014-06-26 13:28:36 -0300

    Correção de bug

--- potm-android-app/src/com/potm_android_app/model/Ti.java
+++ potm-android-app/src/com/potm_android_app/model/Ti.java
@@ -1,4 +1,3 @@
-
 package com.potm_android_app.model;
 
 import org.joda.time.Interval;
@@ -7,91 +6,90 @@ import com.potm_android_app.utils.PotmUtils;
 
 public class Ti implements Comparable<Ti> {
 
-    private String mTitle;
-    private Interval mInterval;
-    private String mDescription;
-    private String mCategory;
-    private final double mProportion;
-    private int mPriority;
+	private String mTitle;
+	private Interval mInterval;
+	private String mDescription;
+	private String mCategory;
+	private final double mProportion;
+	private int mPriority;
 
-    public Ti(String title, Interval interval, String category,
-            String description) {
-        mTitle = title;
-        mInterval = interval;
-        mDescription = "";
-        mCategory = "";
-        mProportion = 0;
-        mPriority = 1;
-    }
+	public Ti(String title, Interval interval, String category,
+			String description) {
+		this.mTitle = title;
+		this.mInterval = interval;
+		this.mDescription = "";
+		this.mCategory = "";
+		this.mProportion = 0;
+		this.mPriority = 1;
+	}
 
-    public Ti(String title, double proportion, int priority) {
-        mTitle = title;
-        mProportion = proportion;
-        mPriority = priority;
-    }
+	public Ti(String title, double proportion, int priority) {
+		this.mTitle = title;
+		this.mProportion = proportion;
+		this.mPriority = priority;
+	}
 
-    public String getTitle() {
-        return mTitle;
-    }
+	public String getTitle() {
+		return this.mTitle;
+	}
 
-    public void setTitle(String title) {
-        mTitle = title;
-    }
+	public void setTitle(String title) {
+		this.mTitle = title;
+	}
 
-    public Interval getInterval() {
-        return mInterval;
-    }
+	public Interval getInterval() {
+		return this.mInterval;
+	}
 
-    public void setInterval(Interval interval) {
-        mInterval = interval;
-    }
+	public void setInterval(Interval interval) {
+		this.mInterval = interval;
+	}
 
-    public String getDescription() {
-        return mDescription;
-    }
+	public String getDescription() {
+		return this.mDescription;
+	}
 
-    public void setDescription(String description) {
-        mDescription = description;
-    }
+	public void setDescription(String description) {
+		this.mDescription = description;
+	}
 
-    public String getCategory() {
-        return mCategory;
-    }
+	public String getCategory() {
+		return this.mCategory;
+	}
 
-    public void setCategory(String category) {
-        mCategory = category;
-    }
+	public void setCategory(String category) {
+		this.mCategory = category;
+	}
 
-    public String getHoursBegin() {
-        return mInterval.getStart().toString(PotmUtils.getDateTimeFormat());
-    }
+	public String getHoursBegin() {
+		return this.mInterval.getStart()
+				.toString(PotmUtils.getDateTimeFormat());
+	}
 
-    public double getProportion() {
-        return mProportion;
-    }
+	public double getProportion() {
+		return this.mProportion;
+	}
 
-    public String getHoursEnd() {
-        return mInterval.getEnd().toString(PotmUtils.getDateTimeFormat());
-    }
+	public String getHoursEnd() {
+		return this.mInterval.getEnd().toString(PotmUtils.getDateTimeFormat());
+	}
 
-    
-    
-    @Override
-    public int compareTo(Ti another) {
-    	if ((Double.compare(this.getProportion(),another.getProportion()) == 0)
-    		 == (this.getProportion() == another.getProportion())	) {
+	@Override
+	public int compareTo(Ti another) {
+		if ((Double.compare(this.getProportion(), another.getProportion()) == 0) == (this
+				.getProportion() == another.getProportion())) {
 			return 0;
-		}else{
-			return Double.compare(this.getProportion(),another.getProportion());			
+		} else {
+			return Double
+					.compare(this.getProportion(), another.getProportion());
 		}
-    } 
-    
-    public int getPriority() {
-        return mPriority;
-    }
+	}
 
-    public void setPriority(int priority) {
-        mPriority = priority;
-    }
+	public int getPriority() {
+		return this.mPriority;
+	}
 
+	public void setPriority(int priority) {
+		this.mPriority = priority;
+	}
 }
